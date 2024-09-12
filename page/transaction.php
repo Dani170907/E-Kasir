2066e508b3afbf431ecbc37933592224ef63b901
Ewhaen <hidayatunnasrullah@gmail.com>
2024-09-12 10:10:00 +0700

    add table body

--- page/transaction.php
+++ page/transaction.php
@@ -13,6 +13,16 @@
                         <th>Opsi</th>
                     </tr>
                 </thead>
+                <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>Dimas Saputro</td>
+                        <td>Tas Vans</td>
+                        <td>2</td>
+                        <td><label for="" class="label label-primary">Sudah</label></td>
+                        <td><a class="btn btn-sm btn-primary" href="?p=transaction_detail">Proses</a></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
