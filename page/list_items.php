f20d3c60fafcef304939700df6b3e4ec7e72b805
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-12 11:19:07 +0700

    update list_items.php

--- page/list_items.php
+++ page/list_items.php
@@ -46,9 +46,9 @@
     <nav aria-label="Page navigation">
         <ul class="pagination">
             <li>
-            <a href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-            </a>
+                <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
             </li>
             <li><a href="#">1</a></li>
             <li><a href="#">2</a></li>
