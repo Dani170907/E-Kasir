6a778834427cf6e1afafa3709118681a5d8ba22d
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-19 09:41:40 +0700

    delete file txt

---
+++ page/edit_item.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <h2>Edit Barang</h2>
+    <div class="col-lg-4">
+        <form action="" class="form">
+            <div class="form-group">
+                <label for="">Nama Barang</label>
+                <input type="text" name="" class="form-control" placeholder="Masukkan Nama Barang">
+            </div>
+            
+            <div class="form-group">
+                <label for="">Harga</label>
+                <input type="number" name="" id="" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-sm-md btn-primary">Simpan</button>
+                <a href="?p=list_item" class="btn btn-md btn-default">Kembali</a>
+            </div>
+        </form>
+    </div>
+</div>
